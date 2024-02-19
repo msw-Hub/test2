75bcb56cd2fd6beb798895a8f200ea14e7a19204
msw-Hub <133941998+msw-Hub@users.noreply.github.com>
2024-02-20 01:25:39 +0900

    1.2

--- src/main/java/io/cloudtype/Demo/controller/PostApiController.java
+++ src/main/java/io/cloudtype/Demo/controller/PostApiController.java
@@ -2,10 +2,7 @@ package io.cloudtype.Demo.controller;
 
 import io.cloudtype.Demo.model.BookRequest;
 import io.cloudtype.Demo.model.UserRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -19,6 +16,8 @@ public class PostApiController {
     }
     //만일 한글이 들어가게 된다면 api tester에서 헤더에 content-encoding을 추가하고 charset=utf-8 을 추가해준다.
     //json 방식 사용시 Request타입을 숫자는 int(x), Integer(o) 해야한다.
+
+    @CrossOrigin(origins = "https://teamswr.store")
     @PostMapping("/user")
     public UserRequest user(
             @RequestBody UserRequest userRequest
