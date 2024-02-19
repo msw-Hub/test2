daf0f6ae4f87636526aa767bf78c34e857ac3a14
msw-Hub <133941998+msw-Hub@users.noreply.github.com>
2024-02-20 02:08:35 +0900

    1.2

--- src/main/java/io/cloudtype/Demo/controller/PostApiController.java
+++ src/main/java/io/cloudtype/Demo/controller/PostApiController.java
@@ -1,8 +1,11 @@
 package io.cloudtype.Demo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cloudtype.Demo.model.BookRequest;
 import io.cloudtype.Demo.model.UserRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -10,21 +13,36 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class PostApiController {
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @PostMapping("/post")
     public String post(
             @RequestBody BookRequest bookRequest
-    ){
-        log.info("Received a POST request: {}", bookRequest);
-        return bookRequest.toString();
+    ) {
+        try {
+            String json = objectMapper.writeValueAsString(bookRequest);
+            log.info("Received a POST request: {}", json);
+            return json;
+        } catch (JsonProcessingException e) {
+            log.error("Error processing JSON", e);
+            return "Error processing JSON";
+        }
     }
 
     @CrossOrigin(origins = "https://teamswr.store")
     @PostMapping("/user")
     public UserRequest user(
             @RequestBody UserRequest userRequest
-    ){
-        log.info("Received a POST request: {}", userRequest);
-        return userRequest;
+    ) {
+        try {
+            String json = objectMapper.writeValueAsString(userRequest);
+            log.info("Received a POST request: {}", json);
+            return userRequest;
+        } catch (JsonProcessingException e) {
+            log.error("Error processing JSON", e);
+            return userRequest;
+        }
     }
 
 }
